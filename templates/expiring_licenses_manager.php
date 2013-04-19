97450294b176686994cc01971bdb6e4a34a02cb9
Remco Tolsma <info@remcotolsma.nl>
2013-04-19 09:42:40 +0200

    Fixed text domain.

--- templates/expiring_licenses_manager.php
+++ templates/expiring_licenses_manager.php
@@ -8,16 +8,16 @@
 			<thead>
 				<tr>
 					<th scope="col" class="manage-column check-column"><input type="checkbox"/></th>
-					<th scope="col" class="manage-column" style="width:3em;"><?php _e( 'ID', 'orbis' ); ?></th>
-					<th scope="col" class="manage-column"><?php _e( 'Company', 'orbis' ); ?></th>
-					<th scope="col" class="manage-column"><?php _e( 'Type', 'orbis' ) ?></th>
-					<th scope="col" class="manage-column"><?php _e( 'Name', 'orbis' ) ?></th>
-					<th scope="col" class="manage-column"><?php _e( 'Activation Date', 'orbis' ) ?></th>
-					<th scope="col" class="manage-column"><?php _e( 'Expiration Date', 'orbis' ) ?></th>
-					<th scope="col" class="manage-column"><?php _e( 'Update Date', 'orbis' ) ?></th>
-					<th scope="col" class="manage-column"><?php _e( 'Price', 'orbis' ) ?></th>
-					<th scope="col" class="manage-column"><?php _e( 'License Key', 'orbis' ) ?></th>
-					<th scope="col" class="manage-column"><?php _e( 'Actions', 'orbis' ) ?></th>
+					<th scope="col" class="manage-column" style="width:3em;"><?php _e( 'ID', 'orbis_subscriptions' ); ?></th>
+					<th scope="col" class="manage-column"><?php _e( 'Company', 'orbis_subscriptions' ); ?></th>
+					<th scope="col" class="manage-column"><?php _e( 'Type', 'orbis_subscriptions' ) ?></th>
+					<th scope="col" class="manage-column"><?php _e( 'Name', 'orbis_subscriptions' ) ?></th>
+					<th scope="col" class="manage-column"><?php _e( 'Activation Date', 'orbis_subscriptions' ) ?></th>
+					<th scope="col" class="manage-column"><?php _e( 'Expiration Date', 'orbis_subscriptions' ) ?></th>
+					<th scope="col" class="manage-column"><?php _e( 'Update Date', 'orbis_subscriptions' ) ?></th>
+					<th scope="col" class="manage-column"><?php _e( 'Price', 'orbis_subscriptions' ) ?></th>
+					<th scope="col" class="manage-column"><?php _e( 'License Key', 'orbis_subscriptions' ) ?></th>
+					<th scope="col" class="manage-column"><?php _e( 'Actions', 'orbis_subscriptions' ) ?></th>
 				</tr>
 			</thead>
 			<tbody>
@@ -28,25 +28,29 @@
 							<td><?php echo $subscription->get_id(); ?></td>
 							<td><?php echo $subscription->get_company_name(); ?></td>
 							<td><?php echo $subscription->get_type_name(); ?></td>
-							<td><?php echo $subscription->get_name(); ?></td>
+							<td>
+								<a href="<?php echo get_permalink( $subscription->get_post_id() ); ?>" target="_blank">
+									<?php echo $subscription->get_name(); ?>
+								</a>
+							</td>
 							<td><?php echo $subscription->get_activation_date()->format( 'd-m-Y' ); ?></td>
 							<td><?php echo $subscription->until_expiration_human(); ?></td>
 							<td><?php echo $subscription->get_update_date()->format( 'd-m-Y' ); ?></td>
 							<td><?php echo $subscription->get_type_price( '&euro;' ); ?></td>
 							<td><?php echo $subscription->get_license_key(); ?></td>
 							<td>
-								<button class="button-primary" name="submit_single" type="submit" value="<?php echo $subscription->get_post_id(); ?>"><?php _e( 'Send Reminder', 'orbis' ); ?></button>
-								<button class="button-secondary" name="submit_extend" type="submit" value="<?php echo $subscription->get_post_id(); ?>"><?php _e( 'Extend License', 'orbis' ); ?></button>
+								<button class="button-primary" name="submit_single" type="submit" value="<?php echo $subscription->get_post_id(); ?>"><?php _e( 'Send Reminder', 'orbis_subscriptions' ); ?></button>
+								<button class="button-secondary" name="submit_extend" type="submit" value="<?php echo $subscription->get_post_id(); ?>"><?php _e( 'Extend License', 'orbis_subscriptions' ); ?></button>
 							</td>
 						</tr>
 					<?php endforeach; ?>
 				<?php else: ?>
 					<tr>
-						<td colspan="10"><?php _e( 'No subscriptions will expire within the next week', 'orbis' ); ?></td>
+						<td colspan="10"><?php _e( 'No subscriptions will expire within the next week', 'orbis_subscriptions' ); ?></td>
 					</tr>
 				<?php endif; ?>
 			</tbody>
 		</table>
-		<?php submit_button( __( 'Send Reminder', 'orbis' ) ); ?>
+		<?php submit_button( __( 'Send Reminder', 'orbis_subscriptions' ) ); ?>
 	</form>
 </div>
