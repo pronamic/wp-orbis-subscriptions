cb19ba74ff0782acaa86d8f75e84c6ad5a338d92
Remco Tolsma <info@remcotolsma.nl>
2014-12-23 13:34:51 +0100

    Added Grunt task to check text domains.

--- Gruntfile.js
+++ Gruntfile.js
@@ -31,6 +31,39 @@ module.exports = function( grunt ) {
 				compare: '=='
 			}
 		},
+
+		// Check textdomain errors
+		checktextdomain: {
+			options:{
+				text_domain: 'orbis_subscriptions',
+				keywords: [
+					'__:1,2d',
+					'_e:1,2d',
+					'_x:1,2c,3d',
+					'esc_html__:1,2d',
+					'esc_html_e:1,2d',
+					'esc_html_x:1,2c,3d',
+					'esc_attr__:1,2d',
+					'esc_attr_e:1,2d',
+					'esc_attr_x:1,2c,3d',
+					'_ex:1,2c,3d',
+					'_n:1,2,4d',
+					'_nx:1,2,4c,5d',
+					'_n_noop:1,2,3d',
+					'_nx_noop:1,2,3c,4d'
+				]
+			},
+			files: {
+				src:  [
+					'**/*.php',
+					'!deploy/**',
+					'!node_modules/**',
+					'!tests/**',
+					'!wp-content/**'
+				],
+				expand: true
+			}
+		},
 		
 		// MakePOT
 		makepot: {
@@ -45,10 +78,11 @@ module.exports = function( grunt ) {
 	} );
 
 	grunt.loadNpmTasks( 'grunt-phplint' );
+	grunt.loadNpmTasks( 'grunt-checktextdomain' );
 	grunt.loadNpmTasks( 'grunt-checkwpversion' );
 	grunt.loadNpmTasks( 'grunt-wp-i18n' );
 
 	// Default task(s).
 	grunt.registerTask( 'default', [ 'phplint', 'checkwpversion' ] );
-	grunt.registerTask( 'pot', [ 'makepot' ] );
+	grunt.registerTask( 'pot', [ 'checktextdomain', 'makepot' ] );
 };
