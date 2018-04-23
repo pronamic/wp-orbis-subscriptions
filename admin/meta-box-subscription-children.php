18a0c1e7baeca91575c37f42b43f447ebaccd31e
Ruben Droogh <rubendroogh@iMac-van-Leo-2.local>
2018-04-23 16:39:38 +0200

    Added list of subscription children links to subscription page.

--- admin/meta-box-subscription-children.php
+++ admin/meta-box-subscription-children.php
@@ -1,19 +1,49 @@
 <?php
-global $post;
+global $wpdb, $post;
 
+$child_subscriptions = $wpdb->get_results( $wpdb->prepare("
+	SELECT
+		post_title,
+		ID,
+		guid
+	FROM
+		$wpdb->posts
+	WHERE
+		post_parent = %d
+			AND
+		post_status = 'publish'
+	;",
+	$post->ID
+) );
 
 ?>
 
-<p class="post-attributes-label-wrapper">
-	<label for="orbis_subscription_id">
-		<b><?php esc_html_e( 'Add Child Subscription', 'orbis_subscriptions' ); ?></b>
-	</label>
-</p>
+<div>
+	<p class="post-attributes-label-wrapper">
+		<label for="orbis_subscription_id">
+			<b><?php esc_html_e( 'Add Child Subscription', 'orbis_subscriptions' ); ?></b>
+		</label>
+	</p>
 
-<?php $person_id = get_post_meta( $post->ID, '_orbis_subscription_child_id', true ); ?>
+	<?php $person_id = get_post_meta( $post->ID, '_orbis_subscription_child_id', true ); ?>
 
-<select id="orbis_subscription_id" name="_orbis_subscription_child_id" class="orbis-id-control orbis-subscription-id-control regular-text">
-	<option>
-		<?php echo esc_html_e( 'Select subscription to connect.', 'orbis_subscriptions' ); ?>
-	</option>
-</select>
+	<select id="orbis_subscription_id" name="_orbis_subscription_child_id" class="orbis-id-control orbis-subscription-id-control regular-text">
+		<option value="<?php echo esc_attr( $subscription_id ); ?>">
+			<?php echo esc_html( 'Choose a subscription', 'orbis_subscriptions' ); ?>
+		</option>
+	</select>
+</div>
+
+<?php if ( $child_subscriptions ) : ?>
+	<p class="post-attributes-label-wrapper">
+		<label for="orbis_subscription_id">
+			<b><?php esc_html_e( 'Child Subscriptions', 'orbis_subscriptions' ); ?></b>
+		</label>
+	</p>
+
+	<ul>
+		<?php foreach ( $child_subscriptions as $child ): ?>
+			<li><a href="<?php echo $child->guid ?>"><?php echo $child->post_title ?></a></li>
+		<?php endforeach ?>
+	</ul>
+<?php endif ?>
