3d7a251a7b12186a771bcc115db97a68a90bd1dc
Remco Tolsma <info@remcotolsma.nl>
2019-05-03 10:19:35 +0200

    Changed panel to card with responsive table.

--- templates/subscription-invoices.php
+++ templates/subscription-invoices.php
@@ -28,62 +28,64 @@ $invoices = $wpdb->get_results( $query );
 
 if ( $invoices ) : ?>
 
-	<div class="panel">
-		<header>
-			<h3><?php esc_html_e( 'Invoices', 'orbis_subscriptions' ); ?></h3>
-		</header>
+	<div class="card mb-3">
+		<div class="card-header">
+			<?php esc_html_e( 'Invoices', 'orbis_subscriptions' ); ?>
+		</div>
 
-		<table class="table table-striped table-bordered">
-			<thead>
-				<tr>
-					<th scope="col"><?php esc_html_e( 'Create Date', 'orbis_subscriptions' ); ?></th>
-					<th scope="col"><?php esc_html_e( 'User', 'orbis_subscriptions' ); ?></th>
-					<th scope="col"><?php esc_html_e( 'Start Date', 'orbis_subscriptions' ); ?></th>
-					<th scope="col"><?php esc_html_e( 'End Date', 'orbis_subscriptions' ); ?></th>
-					<th scope="col"><?php esc_html_e( 'Invoice Number', 'orbis_subscriptions' ); ?></th>
-				</tr>
-			</thead>
-
-			<tbody>
-
-				<?php foreach ( $invoices as $invoice ) : ?>
-
+		<div class="table-responsive">
+			<table class="table table-striped table-bordered mb-0">
+				<thead>
 					<tr>
-						<td>
-							<?php echo esc_html( date_i18n( 'D j M Y H:i:s', strtotime( $invoice->create_date ) ) ); ?>
-						</td>
-						<td>
-							<?php echo esc_html( $invoice->user_display_name ); ?>
-						</td>
-						<td>
-							<?php echo esc_html( date_i18n( 'D j M Y', strtotime( $invoice->start_date ) ) ); ?>
-						</td>
-						<td>
-							<?php echo esc_html( date_i18n( 'D j M Y', strtotime( $invoice->end_date ) ) ); ?>
-						</td>
-						<td>
-							<?php
-
-							$invoice_link = orbis_get_invoice_link( $invoice->invoice_number );
-
-							if ( ! empty( $invoice_link ) ) {
-								printf(
-									'<a href="%s" target="_blank">%s</a>',
-									esc_attr( $invoice_link ),
-									esc_html( $invoice->invoice_number )
-								);
-							} else {
-								echo esc_html( $invoice->invoice_number );
-							}
-
-							?>
-						</td>
+						<th scope="col"><?php esc_html_e( 'Create Date', 'orbis_subscriptions' ); ?></th>
+						<th scope="col"><?php esc_html_e( 'User', 'orbis_subscriptions' ); ?></th>
+						<th scope="col"><?php esc_html_e( 'Start Date', 'orbis_subscriptions' ); ?></th>
+						<th scope="col"><?php esc_html_e( 'End Date', 'orbis_subscriptions' ); ?></th>
+						<th scope="col"><?php esc_html_e( 'Invoice Number', 'orbis_subscriptions' ); ?></th>
 					</tr>
+				</thead>
 
-				<?php endforeach; ?>
+				<tbody>
 
-			</tbody>
-		</table>
+					<?php foreach ( $invoices as $invoice ) : ?>
+
+						<tr>
+							<td>
+								<?php echo esc_html( date_i18n( 'D j M Y H:i:s', strtotime( $invoice->create_date ) ) ); ?>
+							</td>
+							<td>
+								<?php echo esc_html( $invoice->user_display_name ); ?>
+							</td>
+							<td>
+								<?php echo esc_html( date_i18n( 'D j M Y', strtotime( $invoice->start_date ) ) ); ?>
+							</td>
+							<td>
+								<?php echo esc_html( date_i18n( 'D j M Y', strtotime( $invoice->end_date ) ) ); ?>
+							</td>
+							<td>
+								<?php
+
+								$invoice_link = orbis_get_invoice_link( $invoice->invoice_number );
+
+								if ( ! empty( $invoice_link ) ) {
+									printf(
+										'<a href="%s" target="_blank">%s</a>',
+										esc_attr( $invoice_link ),
+										esc_html( $invoice->invoice_number )
+									);
+								} else {
+									echo esc_html( $invoice->invoice_number );
+								}
+
+								?>
+							</td>
+						</tr>
+
+					<?php endforeach; ?>
+
+				</tbody>
+			</table>
+		</div>
 	</div>
 
 <?php endif; ?>
