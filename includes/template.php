8939578fb4ba83b950853df7119925a0689bd976
Remco Tolsma <info@remcotolsma.nl>
2020-02-21 13:57:57 +0100

    No longer add post/body class for cancelled subscriptions, we will add Bootstrap badges to make this clear.

--- includes/template.php
+++ includes/template.php
@@ -56,42 +56,6 @@ function orbis_company_sections_subscriptions( $sections ) {
 add_filter( 'orbis_company_sections', 'orbis_company_sections_subscriptions' );
 
 /**
- * Orbis subscriptions body class
- *
- * @param array $classes
- */
-function orbis_subscriptions_body_class( $classes ) {
-	global $post;
-
-	if ( is_singular( 'orbis_subscription' ) ) {
-		if ( isset( $post->subscription_cancel_date ) && ! empty( $post->subscription_cancel_date ) ) {
-			$classes[] = 'orbis-status-cancelled';
-		}
-	}
-
-	return $classes;
-}
-
-add_filter( 'body_class', 'orbis_subscriptions_body_class' );
-
-/**
- * Orbis subscriptions post class
- *
- * @param array $classes
- */
-function orbis_subscriptions_post_class( $classes ) {
-	global $post;
-
-	if ( isset( $post->subscription_cancel_date ) && ! empty( $post->subscription_cancel_date ) ) {
-		$classes[] = 'orbis-status-cancelled';
-	}
-
-	return $classes;
-}
-
-add_filter( 'post_class', 'orbis_subscriptions_post_class' );
-
-/**
  * Orbis subscription purchase
  */
 function orbis_subscriptions_render_purchase_details() {
