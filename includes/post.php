5f0606c6f4ac01dea3bbdd0e25f058eaaec9a82d
Remco Tolsma <869674+remcotolsma@users.noreply.github.com>
2024-01-29 11:54:54 +0100

    Revert "Add support for Pronamic Moneybird product."
    This reverts commit f9d595dca655cfd7c5da50e3c6a6d24c16ce4cea.
    
    Added in https://github.com/pronamic/wp-orbis-moneybird/commit/f1ee08736e46b4def4cba5c941f6aad27203bf7d.

--- includes/post.php
+++ includes/post.php
@@ -70,12 +70,6 @@ function orbis_subscriptions_create_initial_post_types() {
 				'comments',
 				'thumbnail',
 				'revisions',
-				/**
-				 * Pronamic Moneybird.
-				 * 
-				 * @link https://github.com/pronamic/wp-pronamic-moneybird
-				 */
-				'pronamic_moneybird_product',
 			],
 			'has_archive'   => true,
 			'rewrite'       => [
